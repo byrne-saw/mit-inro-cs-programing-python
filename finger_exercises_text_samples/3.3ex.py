8ba52a1529a333ce03b01e37c4449bf2b687821a
byrne-saw <byrnes.aw@gmail.com>
2021-04-05 11:48:57 -0500

    finished prep for lecture 3

--- finger_exercises_text_samples/3.3ex.py
+++ finger_exercises_text_samples/3.3ex.py
@@ -1,13 +1,16 @@
 x = 123456
 epsilon = 0.01
-step = epsilon**2
 numGuesses = 0
-ans = 0.0
-while abs(ans**2 - x) >= epsilon and ans*ans <= x:
-    ans += step
+low = 0.0
+high = max(1.0, x)
+ans = (high + low)/2.0
+while abs(ans**2 -x) >= epsilon:
+    print('low =', low, 'high =', high, 'ans =', ans)
     numGuesses += 1
+    if ans**2 < x:
+        low = ans
+    else:
+        high = ans
+    ans = (high + low)/2.0
 print('numGuesses =', numGuesses)
-if abs(ans**2 -x) >= epsilon:
-    print('Failed on square root of', x)
-else:
-    print(ans, 'is close to square root of', x)
+print(ans, 'is close to square root of', x)
