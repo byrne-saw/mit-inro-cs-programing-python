ac4d512c5928505bc74aa4ccf7527dd7f480ad68
byrne-saw <byrnes.aw@gmail.com>
2021-04-22 08:55:29 -0500

    refactor ps1a.py, adding global variables and specs

--- problem_sets/solutions/ps1a.py
+++ problem_sets/solutions/ps1a.py
@@ -1,31 +1,33 @@
-# import locale
-
-# locale.setlocale(locale.LC_ALL, '')
+"""
+Problem Set 1, Problem 1a
+4/22/2021
+Andrew Byrnes
+"""
 
 PORTION_DOWN_PAYMENT = 0.25
 R = 0.04
+ANNUAL_SALARY = float(input("Enter your annual salary: "))
+PORTION_SAVED = float(input("Enter the percent of your salary to save, as a decimal: "))
+DEAM_HOME_COST = float(input("Enter the cost of your dream home: "))
+DOWN_PAYMENT = DEAM_HOME_COST * PORTION_DOWN_PAYMENT
+MONTHLY_SALARY = ANNUAL_SALARY / 12
 
 def months_to_save():
-    annual_salary = float(input("Enter your annual salary: "))
-    portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
-    total_cost = float(input("Enter the cost of your dream home: "))
-    
+    """
+    Expects 3 global variables to be defined:
+        DOWN_PAYMENT, MONTHLY_SALARY, PORTION_SAVED
+    Returns the number of months it will take to save for the down payment as int
+    """
     current_savings = 0
-    down_payment = total_cost * PORTION_DOWN_PAYMENT
-    monthly_salary = annual_salary / 12
     num_months = 0
 
-    while current_savings < down_payment:
+    while current_savings < DOWN_PAYMENT:
         current_savings = (
             current_savings 
             + (current_savings * R / 12) 
-            + (monthly_salary * portion_saved)
+            + (MONTHLY_SALARY * PORTION_SAVED)
             )
         num_months += 1
-
-    # total_cost_formatted = locale.currency(total_cost)
-    # down_payment_formatted = locale.currency(down_payment)
-    # current_savings_formated = locale.currency(current_savings)
 
     print(f'Number of months: {num_months}')
 
